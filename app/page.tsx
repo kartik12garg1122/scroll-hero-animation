b9343ebb1eb469469e6bcefe64e28884d898ffe9
Kartik Garg <your-email@gmail.com>
2026-03-06 13:49:31 +0530

    Hero scroll animation project

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "../../components/Hero";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+
+      <div className="h-[200vh] flex items-center justify-center text-3xl">
+        Scroll Down
+      </div>
+
+    </main>
+  );
+}
